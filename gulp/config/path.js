6f440b7fc65c6f3626d4b98478d7c689df16aced
Евгения <euschevlyakova@yandex.ru>
2022-05-26 20:23:15 +0300

    start

---
+++ gulp/config/path.js
@@ -0,0 +1,34 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = 'dist'; // название папки с результатом
+const srcFolder = './src';
+
+export const path = {
+    build: {
+        js: `${buildFolder}/assets/js/`,
+        css: `${buildFolder}/assets/css/`,
+        html: `${buildFolder}/`,
+        images: `${buildFolder}/assets/img/`,
+        files: `${buildFolder}/assets/fonts/`,
+    },
+    src: {
+        js: `${srcFolder}/js/script.js`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        scss: `${srcFolder}/scss/style.scss`,
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/fonts/**/*.*`,
+    },
+    watch: {
+        js: `${srcFolder}/js/**/*.js`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        html: `${ srcFolder }/**/*.html`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+        files: `${ srcFolder }/fonts/**/*.*`,
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder
+}
---
+++ gulp/tasks/scss.js
@@ -0,0 +1,37 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from 'gulp-rename';
+
+import cleanCss from 'gulp-clean-css'; // Сжатие CSS файлов
+import webpcss from 'gulp-webpcss' // Вывод WEBP изображений
+import autoprefixer from 'gulp-autoprefixer'; // Добавляет префиксы для кроссбраузерности
+import groupCssMediaQueries from 'gulp-group-css-media-queries'; // Группировка медиа запросов 
+
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+    return app.gulp.src(app.path.src.scss, { sourcemaps: true })
+        .pipe(app.plugins.replace(/img\//g, '../img/'))
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(groupCssMediaQueries())
+        .pipe(webpcss({
+            webpClass: ".webp",
+            noWebpClass: ".no-webp"
+        }))
+        .pipe(autoprefixer({
+            grid: true,
+            overrideBrowserlist: ["last 3 versions"],
+            cascade: true
+        }))
+        // Расскоментировать если нужен не сжатый дубль файла стилей
+        //.pipe(app.gulp.dest(app.path.build.css))
+        .pipe(cleanCss())
+        .pipe(rename({
+            extname: ".min.css"
+        }))
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(app.plugins.browsersync.stream())
+}
