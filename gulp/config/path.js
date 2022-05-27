1e2cbbc8955284ab8bff718ef8fdf0ef36299d1e
Евгения <euschevlyakova@yandex.ru>
2022-05-27 13:23:37 +0300

    add fonts

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,7 +1,7 @@
 import * as nodePath from 'path';
 const rootFolder = nodePath.basename(nodePath.resolve());
 
-const buildFolder = 'dist'; // название папки с результатом
+const buildFolder = rootFolder; // название папки с результатом
 const srcFolder = './src';
 
 export const path = {
